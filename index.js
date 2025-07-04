e934f8a747a44fd042920be529c0a62974796e0f
Km81 <km81.ryu@icloud.com>
2025-07-04 17:49:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ class SmartThingsWasherPlatform {
     setupValveService(accessory) {
         const deviceId = accessory.context.device.deviceId;
         const service = accessory.getService(Service.Valve) || accessory.addService(Service.Valve, accessory.displayName);
-        service.setCharacteristic(Characteristic.ValveType, Characteristic.ValveType.GENERIC_VALVE);
+        service.setCharacteristic(Characteristic.ValveType, Characteristic.ValveType.IRRIGATION);
 
         this._bindCharacteristic({
             service,
